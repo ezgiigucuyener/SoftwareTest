e6d8669cca41f336f9f4622031341cd55343be37
ezgi <ezgigucuyener@hotmail.com>
2020-06-02 21:29:00 +0300

    initial commit

---
+++ Fractional.java
@@ -0,0 +1,44 @@
+
+public class Fractional {
+	public final long numerator;
+	public final long denominator;
+	public static final String NotANumber = "Not a Number";
+	public static final String PositiveInfinity = "+Infinity";
+	public static final String NegativeInfinity = "-Infinity";
+	
+	Fractional(long numerator, long denominator) {	
+		// student code
+	}
+	
+	Fractional simplify() {
+		// student code
+	}
+	
+	// Not a Number (NaN) 
+	boolean isNaN() {
+		// student code
+	}
+
+	boolean isInfinity() {
+		// student code
+	}
+	
+	int sign(long numerator, long denominator) {
+		// student code
+	}
+	
+	int sign() {
+		// student code
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// student code
+	}
+	
+	@Override
+	public String toString() {
+		// student code
+	}
+
+}
